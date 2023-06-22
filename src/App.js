e706c5537f85193fb19a42f38cea7003eee25f1f
Prudence Muthee <117924809+muthee-p@users.noreply.github.com>
2023-06-22 22:39:02 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -20,14 +20,14 @@ function App() {
 
           <Route path ="/artists/jamesobonyo" element={<JamesObonyo />} />
            <Route path ="/artists/jamesobonyo/jamesobonyoslide" element={<JamesObonyoSlide />} />
-          <Route path ="/artists/mchechu" element={<Mchechu />} />
+          <Route path ="/artwork/mchechu" element={<Mchechu />} />
             <Route path ="/artists/mchechu/mchechuslide" element={<MchechuSlide />} />
           
           
-          <Route path ="/artists/kamyoso" element={<Kamyoso />} />
+          <Route path ="/artwork/kamyoso" element={<Kamyoso />} />
           <Route path ="/artists/kamyoso/kamyososlide" element={<KamyosoSlide />} />
 
-          <Route path ="/artists/bonmwiru" element={<BonMwiru />} />
+          <Route path ="/artwork/bonmwiru" element={<BonMwiru />} />
           <Route path ="/artists/bonmwiru/bonmwiruslide" element={<BonMwiruSlide />} />
         <Route path="contact" element={<Contact />} />
         <Route path="exhibition" element={<Exhibition />} />
